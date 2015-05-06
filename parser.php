775ed10cece9d663385d25c22c00457323d940e0
yrmt <youri.mout@gmail.com>
2015-05-07 01:27:08 +0200

    Stupid parser.

---
+++ parser.php
@@ -0,0 +1,248 @@
+<?php
+
+class Parser {
+	const tags = [
+		'comment' => [
+			'code' => '/\[\s*\!{1}\s*\|\s*(.*?)\s*\]/x',
+			'html' => '<!-- $1 -->',
+			'cont' => false,
+		],
+		'div' => [
+			'code' => '/\[\s*d{1}\s*\|\s*(.*?)\s*\]/',
+			'html' => '<div>$1</div>',
+			'cont' => false,
+		],
+		'bold' => [
+			'code' => '/\[\s*b{1}\s*\|\s*(.*?)\s*\]/',
+			'html' => '<b>$1</b>',
+			'cont' => false,
+		],
+		'h1' => [
+			'code' => '/\[\s*1{1}\s*\|\s*(.*?)\s*\]/',
+			'html' => '<h1>$1</h1>',
+			'cont' => false,
+		],
+		'h2' => [
+			'code' => '/\[\s*2{1}\s*\|\s*(.*?)\s*\]/',
+			'html' => '<h2>$1</h2>',
+			'cont' => false,
+		],
+		'h3' => [
+			'code' => '/\[\s*3{1}\s*\|\s*(.*?)\s*\]/',
+			'html' => '<h3>$1</h3>',
+			'cont' => false,
+		],
+		'p' => [
+			'code' => '/\[\s*p{1}\s*\|\s*(.*?)\s*\]/',
+			'html' => '<p>$1</p>',
+			'cont' => false,
+		],
+		'i' => [
+			'code' => '/\[\s*i{1}\s*\|\s*(.*?)\s*\]/',
+			'html' => '<i>$1</i>',
+			'cont' => false,
+		],
+		'u' => [
+			'code' => '/\[\s*u{1}\s*\|\s*(.*?)\s*\]/',
+			'html' => '<u>$1</u>',
+			'cont' => false,
+		],
+		'newline' => [
+			'code' => '/\[\s*n{1}\s*\]/',
+			'html' => '<br>',
+			'cont' => false,
+		],
+		'hr' => [
+			'code' => '/\[\s*h{1}\s*\]/',
+			'html' => '<hr>',
+			'cont' => false,
+		],
+		'a' => [
+			'code' => '/\[\s*a{1}\s*\|\s*(.*?)\|\s*(.*?)\]/',
+			'html' => '<a href="$1">$2</a>',
+			'cont' => false,
+		],
+		'img' => [
+			'code' => '/\[\s*img{1}\s*\|\s*(.*?)\s*\|\s*(.*?)\s*\]/',
+			'html' => '<img src="$1" alt="$2">',
+			'cont' => false,
+		],
+		'ul' => [
+			'code' => '/\[\s*ul{1}\s*\|\s*(.*)\s*\]/',
+			'html' => '<ul>$1</ul>',
+			'cont' => true,
+		],
+		'ol' => [
+			'code' => '/\[\s*(ol){1}\s*\|\s*(.*?)\s*\]/',
+			'html' => '<ol>$2</ol>',
+			'cont' => true,
+		],
+		'ol2' => [
+			'code' => '/\[\s*(ol_){1}\s*\|\s*(.*?)\s*\|\s*(.*?)\s*\|\s*(.*?)\s*\]/',
+			'html' => '<ol type="$2" start="$3">$4</ol>',
+			'cont' => true,
+		],
+		'spancolor' => [
+			'code' => '/\[\s*(#[A-f0-9]{6}|#[A-f0-9]{3})\s*\|\s*(.*)\s*\]/',
+			'html' => '<span style="color: $1;">$2</span>',
+			'cont' => false,
+		],
+		'spanbg' => [
+			'code' => '/\[\s*bg{1}\s*\|\s*(#[A-f0-9]{6}|#[A-f0-9]{3})\s*\|\s*(.*)\s*\]/',
+			'html' => '<span style="background: $1;">$2</span>',
+			'cont' => false,
+		],
+		'divbg' => [
+			'code' => '/\[\s*div{1}\s*\|\s*(#[A-f0-9]{6}|#[A-f0-9]{3})\s*\|\s*(.*?)\s*\]/',
+			'html' => '<div style="background: $1;">$2</div>',
+			'cont' => false,
+		],
+		'divbgimg' => [
+			'code' => '/\[\s*div{1}\s*\|\s*(.*?)\s*\|\s*(.*)\s*\]/',
+			'html' => '<div style="background: url($1);">$2</div>',
+			'cont' => false,
+		],
+		'keyword' => [
+			'code' => '/\[\[\s*(.*?)\s*\]\]/',
+			'html' => '<a target="_blank" href="keyword=nonekeyword=pageKeyword">$1</a>',
+			'cont' => false,
+		],
+		's' => [ // ??
+			'code' => '/\[\s*s{1}\s*\|\s*(.*?)\s*\]/',
+			'html' => '<span>$1</span>',
+			'cont' => false,
+		],
+	];
+
+	public static function do_li($elements) {
+		$string = "";
+		foreach ($elements as $li) {
+			// if <li> is a list itself XXX todo ol ol2
+			// embedded ul's
+			if (preg_match('/\s*<ul>\s*(.*?)\s*<\/ul>\s*/', $li)) {
+				$li = preg_replace('/\s*<ul>\s*(.*?)\s*<\/ul>\s*/', '<ul><li>$1</li></ul>', $li);
+				$string .= $li;
+			// embedded ol's
+			} else if (preg_match('/\s*<ol>\s*(.*?)\s*<\/ol>\s*/', $li)) {
+				$li = preg_replace('/\s*<ol>\s*(.*?)\s*<\/ol>\s*/', '<ol><li>$1</li></ol>', $li);
+				$string .= $li;
+			} else {
+				if (preg_match('/<li>{2}/', $li)) {
+					$string .= $i;
+				} else {
+					$string .= '<li>' . $li . '</li>';
+				}
+			}
+		}
+		return $string;
+	}
+
+	public static function dolist($in, $str, $tag) {
+		$list = explode('|', $str);
+		$listitems = self::do_li($list);
+		$in = str_replace($str, $listitems, $in);
+		return $in;
+	}
+
+	public static function getBrackets($str, &$out) {
+		$length = strlen($str);
+		$stack  = array();
+		$result = array();
+
+		for ($i = 0; $i < $length; $i++) {
+			$open = -1;
+			if ($str[$i] === '[') {
+				if ($i > 0) {
+				 	if ($str[$i - 1] !== '\\') {
+				 		$stack[] = $i;
+				 	}
+				} else if ($i === 0) {
+					$stack[] = $i;
+				}
+			}
+
+			if ($str[$i] === ']') {
+				if ($i > 0) {
+			 		if ($str[$i - 1] !== '\\') {
+						$open = array_pop($stack);
+				 	}
+				}
+				if ($open !== -1) {
+					$result[] = substr($str, $open, $i - $open + 1);
+				}
+				
+			}
+		}
+		$out = $result;
+  		return count($result);
+	}
+
+	public static function get($in) {
+		// first match words so our brackets searcher doesn't get confused
+		while (preg_match(self::tags['keyword']['code'], $in)) {
+			$in = preg_replace(self::tags['keyword']['code'], self::tags['keyword']['html'], $in);
+		}
+
+		// other tags
+		$bcount = self::getBrackets($in, $out);
+		for ($x = 0; $x < $bcount; $x++) {
+			if ($bcount > 1) {
+				self::getBrackets($in, $out);
+			}
+			$old = $new = reset($out);
+			foreach (self::tags as $name => $parser) {
+			 	$new = preg_replace($parser['code'], $parser['html'], $new);
+			}
+			$in = str_replace($old, $new, $in);
+		}
+
+		// ul tags
+		$count = preg_match_all('/<ul>(.*)<\/ul>/', $in, $matches);
+		if ($count > 0) {
+			foreach ($matches[1] as $string) {
+				$in = self::dolist($in, $string, 'ul');
+				$innercount = preg_match('/<ul>(.*?)<\/ul>/', $string, $innermatches);
+				if ($innercount > 0) {
+					$in = self::dolist($in, '<ul>' . $innermatches[1], 'ul');
+				}
+			}
+		}
+
+		// ol tags
+		$count = preg_match_all('/<ol>(.*)<\/ol>/', $in, $matches);
+		if ($count > 0) {
+			foreach ($matches[1] as $string) {
+				$in = self::dolist($in, $string, 'ol');
+				$innercount = preg_match('/<ol>(.*?)<\/ol>/', $string, $innermatches);
+				if ($innercount > 0) {
+					$in = self::dolist($in, '<ol>' . $innermatches[1], 'ol');
+				}
+			}
+		}
+
+		// ol_ tags
+		$count = preg_match_all('/<ol.*?\">(.*)<\/ol>/', $in, $matches);
+		if ($count > 0) {
+			foreach ($matches[1] as $string) {
+				$in = self::dolist($in, $string, 'ol');
+				// $innercount = preg_match('/<ol(.*?)>(.*?)<\/ol>/', $string, $innermatches);
+				// var_dump($innermatches);
+				// if ($innercount > 0) {
+				// 	$in = self::dolist($in, '<ol>' . $innermatches[1], 'ol');
+				// }
+			}
+		}
+
+		// special chars
+		$in = preg_replace('/\\\\/', '',  $in);
+		// sometimes I get two <li> for some reason... ugly fix.
+		$in = preg_replace('/(<li>){2,3}/', '<li>', $in);
+		return $in;
+	}
+}
+
+$multitest = file_get_contents('toparse');
+//print Parser::get($multitest);
+file_put_contents('index.html', Parser::get($multitest));
+
+?>
